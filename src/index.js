751cd268ff61d38488fb0e35bfd120b72ff79072
Marina1232 <marina.saiko.18@gmail.com>
2022-03-23 21:35:22 +0300

    feat: add task's solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let i = 0;
+    let result = "";
+    let absNum = Math.abs(n);
+    let strNumber = absNum.toString();
+
+    while (i < strNumber.length) {
+        result = `${strNumber[i]}${result}`;
+        i += 1;
+    }
+    return result;
+};
